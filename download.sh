d78b7b185237393c81e948f7abcf0b2330d88894
Phil <phil@parsons.uk.com>
2013-10-27 22:52:48 +0100

    updated download to use hard drive

--- download.sh
+++ download.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-cd $HOME/KBEDC
+cd /mnt
 mkdir data
 cd data
-wget -x -nd --load-cookies ../cookies.txt http://www.kaggle.com/c/belkin-energy-disaggregation-competition/download/H1.zip
+wget -x -nd --load-cookies $HOME/KBEDC/cookies.txt http://www.kaggle.com/c/belkin-energy-disaggregation-competition/download/H1.zip
 unzip H1.zip
 rm H1.zip
-# wget -x -nd --load-cookies ../cookies.txt http://www.kaggle.com/c/belkin-energy-disaggregation-competition/download/H2.zip
-# wget -x -nd --load-cookies ../cookies.txt http://www.kaggle.com/c/belkin-energy-disaggregation-competition/download/H3.zip
-# wget -x -nd --load-cookies ../cookies.txt http://www.kaggle.com/c/belkin-energy-disaggregation-competition/download/H4.zip
-wget -x -nd --load-cookies ../cookies.txt http://www.kaggle.com/c/belkin-energy-disaggregation-competition/download/SampleSubmission.csv
+wget -x -nd --load-cookies $HOME/KBEDC/cookies.txt http://www.kaggle.com/c/belkin-energy-disaggregation-competition/download/H2.zip
+unzip H3.zip
+rm H3.zip
+wget -x -nd --load-cookies $HOME/KBEDC/cookies.txt http://www.kaggle.com/c/belkin-energy-disaggregation-competition/download/H3.zip
+unzip H3.zip
+rm H3.zip
+wget -x -nd --load-cookies $HOME/KBEDC/cookies.txt http://www.kaggle.com/c/belkin-energy-disaggregation-competition/download/H4.zip
+unzip H4.zip
+rm H4.zip
+wget -x -nd --load-cookies $HOME/KBEDC/cookies.txt http://www.kaggle.com/c/belkin-energy-disaggregation-competition/download/SampleSubmission.csv
+
+cd $HOME/KBEDC
+ln -s /mnt/data .
