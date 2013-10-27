93ff352d9f6071732340b22ebb2114495a96de52
Phil <phil@parsons.uk.com>
2013-10-27 18:49:37 +0100

    updated data downloader

--- download.sh
+++ download.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-screen
-mkdir data
+cd $HOME/KBEDC
+if [ -d ./data/ ]; then
+	mkdir data
+fi
 cd data
-wget -x --load-cookies cookies.txt http://www.kaggle.com/c/belkin-energy-disaggregation-competition/download/H1.zip
-wget -x --load-cookies cookies.txt http://www.kaggle.com/c/belkin-energy-disaggregation-competition/download/H2.zip
-wget -x --load-cookies cookies.txt http://www.kaggle.com/c/belkin-energy-disaggregation-competition/download/H3.zip
-wget -x --load-cookies cookies.txt http://www.kaggle.com/c/belkin-energy-disaggregation-competition/download/H4.zip
-wget -x --load-cookies cookies.txt http://www.kaggle.com/c/belkin-energy-disaggregation-competition/download/SampleSubmission.csv
+wget -x --load-cookies ../cookies.txt http://www.kaggle.com/c/belkin-energy-disaggregation-competition/download/H1.zip
+wget -x --load-cookies ../cookies.txt http://www.kaggle.com/c/belkin-energy-disaggregation-competition/download/H2.zip
+wget -x --load-cookies ../cookies.txt http://www.kaggle.com/c/belkin-energy-disaggregation-competition/download/H3.zip
+wget -x --load-cookies ../cookies.txt http://www.kaggle.com/c/belkin-energy-disaggregation-competition/download/H4.zip
+wget -x --load-cookies ../cookies.txt http://www.kaggle.com/c/belkin-energy-disaggregation-competition/download/SampleSubmission.csv
