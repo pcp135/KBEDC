23118a568d407ae2c26421881eca869e5de1256c
Phil <phil@parsons.uk.com>
2013-10-27 22:34:35 +0100

    improved download script

--- download.sh
+++ download.sh
@@ -6,7 +6,9 @@ if [ -d ./data/ ]; then
 fi
 cd data
 wget -x -nd --load-cookies ../cookies.txt http://www.kaggle.com/c/belkin-energy-disaggregation-competition/download/H1.zip
-wget -x -nd --load-cookies ../cookies.txt http://www.kaggle.com/c/belkin-energy-disaggregation-competition/download/H2.zip
-wget -x -nd --load-cookies ../cookies.txt http://www.kaggle.com/c/belkin-energy-disaggregation-competition/download/H3.zip
-wget -x -nd --load-cookies ../cookies.txt http://www.kaggle.com/c/belkin-energy-disaggregation-competition/download/H4.zip
+unzip H1.zip
+rm H1.zip
+# wget -x -nd --load-cookies ../cookies.txt http://www.kaggle.com/c/belkin-energy-disaggregation-competition/download/H2.zip
+# wget -x -nd --load-cookies ../cookies.txt http://www.kaggle.com/c/belkin-energy-disaggregation-competition/download/H3.zip
+# wget -x -nd --load-cookies ../cookies.txt http://www.kaggle.com/c/belkin-energy-disaggregation-competition/download/H4.zip
 wget -x -nd --load-cookies ../cookies.txt http://www.kaggle.com/c/belkin-energy-disaggregation-competition/download/SampleSubmission.csv
