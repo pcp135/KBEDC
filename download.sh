2aa2fec3cd299bb2be8b3a4be0b8d56eba48d16e
Phil <phil@parsons.uk.com>
2013-10-28 11:34:10 +0100

    bugfixes

--- download.sh
+++ download.sh
@@ -3,13 +3,16 @@
 cd /mnt
 sudo mkdir data
 sudo chown ubuntu data
+cd $HOME/KBEDC
+ln -s /mnt/data .
 cd data
+
 wget -x -nd --load-cookies $HOME/KBEDC/cookies.txt http://www.kaggle.com/c/belkin-energy-disaggregation-competition/download/H1.zip
 unzip H1.zip
 rm H1.zip
 wget -x -nd --load-cookies $HOME/KBEDC/cookies.txt http://www.kaggle.com/c/belkin-energy-disaggregation-competition/download/H2.zip
-unzip H3.zip
-rm H3.zip
+unzip H2.zip
+rm H2.zip
 wget -x -nd --load-cookies $HOME/KBEDC/cookies.txt http://www.kaggle.com/c/belkin-energy-disaggregation-competition/download/H3.zip
 unzip H3.zip
 rm H3.zip
@@ -18,5 +21,3 @@ unzip H4.zip
 rm H4.zip
 wget -x -nd --load-cookies $HOME/KBEDC/cookies.txt http://www.kaggle.com/c/belkin-energy-disaggregation-competition/download/SampleSubmission.csv
 
-cd $HOME/KBEDC
-ln -s /mnt/data .
